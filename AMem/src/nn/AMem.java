559f3773f4f7f8dbbe0d69a2c1b85d057fecd5f5
peiva <peiva@ubuntu-MainLaptop>
2019-06-11 16:42:15 +0200

    setup, examples

--- AMem/src/nn/AMem.java
+++ AMem/src/nn/AMem.java
@@ -1,8 +1,9 @@
 package nn;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
-public class AMem  extends PApplet{
+public class AMem extends PApplet{
 
 	public static void main(String[] args) {
 		PApplet.main("nn.AMem");
@@ -13,17 +14,31 @@ public class AMem  extends PApplet{
 	static final int windowSizeY = 640;
 	
 	
+	PImage testImage;
+	
 	public void settings() {
 		size(windowSizeX, windowSizeY);
 		
 	}
 	
+	float xoff = 0;
+	
 	public void setup() {
+		testImage = loadImage("/home/peiva/Downloads/nnTestImage_small.jpeg");
+		testImage.loadPixels();
 		
+	
 	}
 	
 	public void draw() {
-		square(320, 320, 400);
+		image(testImage, 0, 0);
+		background(255);
+		/*
+		xoff = xoff + .01f;
+		float n = noise(xoff) * width;
+		line(n, 0, n, height);
+		*/
+		println(noise(random(1), random(1))); //random value between 0 and 1 for every coordinate
 		
 	}
 
